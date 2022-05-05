aa89d33a714e8d76fe02e9198df38bed6e20fdc7
carloscmor <100674357+carloscmor@users.noreply.github.com>
2022-05-05 12:17:32 +0200

    Update Jugador.java

--- código/prPFentertainment/src/main/java/Modelo/Jugador.java
+++ código/prPFentertainment/src/main/java/Modelo/Jugador.java
@@ -1,4 +1,15 @@
 package Modelo;
-
 public class Jugador {
+    private String nombre;
+    private int vidas, puntuacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
 }
