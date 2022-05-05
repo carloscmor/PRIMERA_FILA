0d7136ac35be545089babfff8a17ea68196ac1f4
Dani-gr <dani.gr@uma.es>
2022-05-05 11:46:43 +0200

    Listado de argumentos de clase Pregunta

--- código/prPFentertainment/src/main/java/Modelo/Pregunta.java
+++ código/prPFentertainment/src/main/java/Modelo/Pregunta.java
@@ -2,4 +2,19 @@ package Modelo;
 
 public class Pregunta {
 
+
+
+    /*
+    Categoría
+    Pregunta
+    Solución
+    Dificultad
+    Opciones
+    Timer timerPregunta
+
+    Pregunta()
+
+    validar()
+
+     */
 }
