c5635bdd6df08b83e708d81c75e5a689e2d599f4
javild01 <100673854+javild01@users.noreply.github.com>
2022-05-05 11:54:30 +0200

    Add files via upload

--- código/prPFentertainment/src/main/java/Modelo/Pregunta.java
+++ código/prPFentertainment/src/main/java/Modelo/Pregunta.java
@@ -1,5 +1,36 @@
 package Modelo;
 
 public class Pregunta {
-
+	public Pregunta(String pregunta, String solucion) {
+		this.pregunta = pregunta;
+		this.solucion = solucion;
+	}
+	String categoria;
+	String pregunta;
+	String solucion;
+	String dificultad;
+	String opciones;
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDificultad() {
+		return dificultad;
+	}
+	public void setDificultad(String dificultad) {
+		this.dificultad = dificultad;
+	}
+	public String getOpciones() {
+		return opciones;
+	}
+	public void setOpciones(String opciones) {
+		this.opciones = opciones;
+	}
+	
+	public boolean validar(String resp) {
+		return solucion.equals(resp);
+	}
+	
 }
