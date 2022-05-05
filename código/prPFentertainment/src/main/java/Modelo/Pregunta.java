cb79f74a31a11b0b8c66a5ac3ef520ea6afcb0ab
Nu36 <nurirt36@uma.es>
2022-05-05 11:57:59 +0200

    creación de variables

--- código/prPFentertainment/src/main/java/Modelo/Pregunta.java
+++ código/prPFentertainment/src/main/java/Modelo/Pregunta.java
@@ -1,5 +1,14 @@
 package Modelo;
+import java.util.Set;
 
 public class Pregunta {
+    private String categoria;
+    private String pregunta;
+    private String solucion;
+    private int dificultad; // 1 facil 2 medio 3 dificil
+    private Set<String> opciones;
 
+    public boolean validar(String respuesta){
+        return respuesta.equalsIgnoreCase(solucion);
+    }
 }
