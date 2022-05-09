08d8014252086d8ae66f0eafc7073000592d7f20
TetrisRubik <fpablo960@gmail.com>
2022-05-09 16:59:53 +0100

    Comentarios en Dado Solo unas aclaraciones para entender claramente cada parte. Comentarios con ¡Depuración! indica que la función está destinada para las pruebas.

--- código/prPFentertainment/src/main/java/Modelo/Dado.java
+++ código/prPFentertainment/src/main/java/Modelo/Dado.java
@@ -3,22 +3,25 @@ package Modelo;
 import java.util.Random;
 
 public class Dado {
-	private Random rd;
-	private int valor;
+	private Random rd; // Clase que proporciona el siguiente resultado.
+	private int valor; // Valor actual del dado (cara superior).
 
 	public Dado() {
 		rd = new Random();
-		valor = 0;
+		valor = 0; // 0 indica un dado sin lanzar. ¿Cambiar a aleatorio?
 	}
 
+	// Devuelve el valor actual del dado. ¡Depuración!
 	public int valor() {
 		return valor;
 	}
 
+	// Cambia el valor del dado. ¡Depuración!
 	public void asignar(int num) {
 		valor = num;
 	}
 
+	// Asigna a valor el siguiente resultado.
 	public void tirar() {
 		valor = rd.nextInt(6) + 1;
 	}
