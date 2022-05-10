f7a7b3a918704d7089c8fe2a77f5e7479d2107e8
jLanceta <100673997+jLanceta@users.noreply.github.com>
2022-05-10 10:12:26 +0200

    Clase Temporizador Actualizada + Prueba JUnit
    Hay un nuevo método añadido a la clase Temporizador. También añado las pruebas JUnit de la clase.

--- Temporizador.java
+++ Temporizador.java
@@ -1,4 +1,4 @@
-package TemporizadorPruebaSemaforo;
+package Temporizador;
 
 public class Temporizador extends Thread {
 
@@ -8,7 +8,11 @@ public class Temporizador extends Thread {
 	private boolean respondido;
 	public boolean fin;
 
+	//Constructor
 	public Temporizador(int n) {
+		if (n <= 0) {
+			throw new IllegalArgumentException("Valor incorrecto.");
+		}
 		this.inicio = n;
 		this.tiempo = n;
 		agotado = false;
@@ -16,20 +20,29 @@ public class Temporizador extends Thread {
 		fin = false;
 	}
 
+	//Devuelve el estado del temporizador, es decir, si esta agotado o no
 	public boolean getResultado() {
 		return this.agotado;
 	}
+	
+	//Devuelve la cantidad de tiempo del temporizador. No usar mientras el temporizador esta corriendo
+	public int getTiempo() {
+		return tiempo;
+	}
 
+	//Para el temporizador
 	public void parar() {
 		fin = true;
 	}
 
+	//Reinicia el temporizador, reinicializando las variables
 	public void reiniciar() {
 		this.tiempo = inicio + 1;
 		this.agotado = false;
 		this.respondido = false;
 	}
 
+	//Inicia el temporizador
 	public void run() {
 		while (!fin) {
 			if (tiempo >= 0)
