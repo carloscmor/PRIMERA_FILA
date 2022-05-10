b28756b6c2b66c98c7dc1949c776dfa494712936
Nu36 <100622207+Nu36@users.noreply.github.com>
2022-05-10 10:22:26 +0200

    Update PartidaDesafio.java

--- código/prPFentertainment/src/main/java/Modelo/PartidaDesafio.java
+++ código/prPFentertainment/src/main/java/Modelo/PartidaDesafio.java
@@ -1,16 +1,23 @@
 package Modelo;
 
+private Tablero t;
+private Jugador j;
+
 public class PartidaDesafio implements Partida{
     public PartidaDesafio() {
     }
 
     @Override
     public void empezar() {
-
+        t = new Tablero(d);
+        j = new Jugador();
+        j.setvidas(5);
     }
 
     @Override
     public void terminar() {
-
+        if (j.getvidas() == 0){
+            //hay que completar
+        }
     }
 }
