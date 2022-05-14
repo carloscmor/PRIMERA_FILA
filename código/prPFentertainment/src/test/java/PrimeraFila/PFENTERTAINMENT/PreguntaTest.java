514923a34ac71eeb144fc04681fa5a326268e70f
guillermoTell444 <100612552+guillermoTell444@users.noreply.github.com>
2022-05-14 09:19:28 +0200

    Update PreguntaTest.java

--- código/prPFentertainment/src/test/java/PrimeraFila/PFENTERTAINMENT/PreguntaTest.java
+++ código/prPFentertainment/src/test/java/PrimeraFila/PFENTERTAINMENT/PreguntaTest.java
@@ -1,4 +1,4 @@
-package practicaMockito.practicaSensores;
+package PrimeraFila.PFENTERTAINMENT;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
