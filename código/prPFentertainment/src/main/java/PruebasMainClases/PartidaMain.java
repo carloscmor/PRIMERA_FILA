0c8f0dc58286329c842d0acc7236c3043db3d8c6
carloscmor <100674357+carloscmor@users.noreply.github.com>
2022-05-19 12:26:43 +0200

    Partida Y excepciones

---
+++ código/prPFentertainment/src/main/java/PruebasMainClases/PartidaMain.java
@@ -0,0 +1,12 @@
+package PruebasMainClases;
+
+import Modelo.Partida;
+
+public class PartidaMain {
+
+	public static void main(String[] args) {
+		Partida p = new Partida("Carlos");
+		System.out.println(p);
+	}
+
+}
