d7f3615ac881999dccf451f0c6613df2ad854a78
Pablo <pablofs02@uma.es>
2022-05-19 11:17:18 +0200

    Estilo Logotipo Ahora con el cambio de tema se cambia el logotipo correspondiente.

--- código/prPFentertainment/src/main/resources/static/funciones/configuración/tema.js
+++ código/prPFentertainment/src/main/resources/static/funciones/configuración/tema.js
@@ -1,21 +1,25 @@
 //* Función que cambia el tema.
-export default function cambiar_tema(nodo, accionador) {
+export default function cambiar_tema(nodo, accionador, logo) {
 	const tema_oscuro = "estilos/oscuro.css";
 	const tema_claro = "estilos/claro.css";
 	const icono_oscuro = "archivos/vectores/modo_oscuro.svg";
 	const icono_claro = "archivos/vectores/modo_claro.svg";
 	const alternativa_oscuro = "Cambiar a modo oscuro";
 	const alternativa_claro = "Cambiar a modo claro";
+	const logo_oscuro = "archivos/Logo oscuro.png";
+	const logo_claro = "archivos/logo claro.png";
 
 	if (nodo.getAttribute("href") === tema_oscuro) {
 		nodo.setAttribute("href", tema_claro);
 		accionador.setAttribute("src", icono_oscuro);
 		accionador.setAttribute("alt", alternativa_oscuro);
 		accionador.setAttribute("title", alternativa_oscuro);
+		logo.setAttribute("src", logo_claro);
 	} else {
 		nodo.setAttribute("href", tema_oscuro);
 		accionador.setAttribute("src", icono_claro);
 		accionador.setAttribute("alt", alternativa_claro);
 		accionador.setAttribute("title", alternativa_claro);
+		logo.setAttribute("src", logo_oscuro);
 	}
 }
--- código/prPFentertainment/src/main/resources/static/funciones/principal.js
+++ código/prPFentertainment/src/main/resources/static/funciones/principal.js
@@ -3,8 +3,9 @@ import cambiar_tema from "./configuración/tema.js";
 
 const botón_tema = document.getElementById("botón_tema");
 const nodo_tema = document.getElementById("tema_css");
+const nodo_logo = document.getElementById("logotipo");
 
 // Botón para cambiar el tema.
 botón_tema.addEventListener("click", () => {
-	cambiar_tema(nodo_tema, botón_tema);
+	cambiar_tema(nodo_tema, botón_tema, nodo_logo);
 });
