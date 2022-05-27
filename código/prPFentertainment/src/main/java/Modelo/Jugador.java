172b05ce1e5c28474124bc0d2258bf5aba1fc67f
Dani-gr <dani.gr@uma.es>
2022-05-27 22:25:28 +0200

    Fixes varios 2
    1. Más cambios de identación y de uso de bloques de código singular.
    2. Eliminación de variable auxiliar en Jugador.toString().
    3. Eliminadas redundancias varias en clase Partida.

--- código/prPFentertainment/src/main/java/Modelo/Jugador.java
+++ código/prPFentertainment/src/main/java/Modelo/Jugador.java
@@ -3,55 +3,54 @@ package Modelo;
 import java.util.StringJoiner;
 
 public class Jugador {
-	private String nombre;
-	private int vidas, puntuacion;
+    private String nombre;
+    private int vidas, puntuacion;
 
-	public Jugador(String nombre, int vidas) {
-		this(nombre, vidas, 0);
-	}
+    public Jugador(String nombre, int vidas) {
+        this(nombre, vidas, 0);
+    }
 
-	public Jugador(String nombre, int vidas, int puntuacion) {
-		this.nombre = nombre;
-		this.vidas = vidas;
-		this.puntuacion = puntuacion;
-	}
+    public Jugador(String nombre, int vidas, int puntuacion) {
+        this.nombre = nombre;
+        this.vidas = vidas;
+        this.puntuacion = puntuacion;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public int getVidas() {
-		return vidas;
-	}
+    public int getVidas() {
+        return vidas;
+    }
 
-	public void setVidas(int vidas) {
-		if (vidas >= 0)
-			this.vidas = vidas;
-	}
+    public void setVidas(int vidas) {
+        if (vidas >= 0)
+            this.vidas = vidas;
+    }
 
-	public int getPuntuacion() {
-		return puntuacion;
-	}
+    public int getPuntuacion() {
+        return puntuacion;
+    }
 
-	public void setPuntuacion(int puntuacion) {
-		if (puntuacion >= 0)
-			this.puntuacion = puntuacion;
-	}
+    public void setPuntuacion(int puntuacion) {
+        if (puntuacion >= 0) this.puntuacion = puntuacion;
+    }
 
-	public void incrementaPuntuacion(int p) {
-		puntuacion += p;
-	}
+    public void incrementaPuntuacion(int p) {
+        puntuacion += p;
+    }
 
-	@Override
-	public String toString() {
-		StringJoiner str = new StringJoiner(", ", "(", ")");
-		str.add(nombre);
-		str.add("Vidas= " + Integer.toString(vidas));
-		str.add("Puntuación= " + Integer.toString(puntuacion));
-		return str.toString();
-	}
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", "(", ")")
+                .add(nombre)
+                .add("Vidas= " + vidas)
+                .add("Puntuación= " + puntuacion)
+                .toString();
+    }
 }
