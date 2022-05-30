597f3843361c6938f93f7e0f23329a95e08b44c5
carloscmor <carlos051247@uma.es>
2022-05-31 00:14:39 +0200

    Elementos de la interfaz

--- código/prPFentertainment/src/main/java/GUI/App.java
+++ código/prPFentertainment/src/main/java/GUI/App.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import java.awt.Toolkit;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -17,11 +18,23 @@ import java.awt.event.ActionEvent;
 import java.awt.Component;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.ScrollPaneConstants;
+
+import java.awt.TextArea;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import java.awt.SystemColor;
+import java.awt.Canvas;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import java.awt.ScrollPane;
 
 public class App {
 
 	private JFrame frmSabelotodo;
-	private JTextField textField;
+	private JTextField insertaJugador;
+	private JTextField timeLog;
 
 	/**
 	 * Launch the application.
@@ -60,66 +73,222 @@ public class App {
 		frmSabelotodo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmSabelotodo.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("SALIR");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton EndButton = new JButton("TERMINAR");
+		EndButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(1164, 584, 89, 23);
-		frmSabelotodo.getContentPane().add(btnNewButton);
+		EndButton.setBounds(1134, 584, 119, 23);
+		frmSabelotodo.getContentPane().add(EndButton);
 		
-		JButton btnNewButton_1 = new JButton("DESAFÍO");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton modoDesafio = new JButton("DESAFÍO");
+		modoDesafio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_1.setBounds(939, 69, 89, 23);
-		frmSabelotodo.getContentPane().add(btnNewButton_1);
+		modoDesafio.setBounds(846, 58, 99, 42);
+		frmSabelotodo.getContentPane().add(modoDesafio);
 		
-		JButton btnNewButton_2 = new JButton("RÁPIDA");
-		btnNewButton_2.setBounds(1038, 69, 89, 23);
-		frmSabelotodo.getContentPane().add(btnNewButton_2);
+		JButton modoRapida = new JButton("RÁPIDA");
+		modoRapida.setBounds(955, 58, 90, 42);
+		frmSabelotodo.getContentPane().add(modoRapida);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("src\\main\\resources\\static\\archivos\\logo\\logo_oscuro_chico.png"));
 		lblNewLabel.setBounds(10, 11, 133, 139);
 		frmSabelotodo.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("SABELOTODO");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 46));
-		lblNewLabel_1.setBounds(153, 33, 332, 67);
-		frmSabelotodo.getContentPane().add(lblNewLabel_1);
+		JLabel nombreAPP = new JLabel("SABELOTODO");
+		nombreAPP.setFont(new Font("Tahoma", Font.PLAIN, 46));
+		nombreAPP.setBounds(153, 33, 332, 67);
+		frmSabelotodo.getContentPane().add(nombreAPP);
 		
-		JLabel lblNewLabel_2 = new JLabel("EL JUEGO QUE PONE A PRUEBA TUS CONOCIMIENTOS");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_2.setBounds(144, 93, 360, 14);
-		frmSabelotodo.getContentPane().add(lblNewLabel_2);
+		JLabel eslogan = new JLabel("EL JUEGO QUE PONE A PRUEBA TUS CONOCIMIENTOS");
+		eslogan.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		eslogan.setBounds(144, 93, 360, 14);
+		frmSabelotodo.getContentPane().add(eslogan);
 		
-		JLabel lblNewLabel_3 = new JLabel("Selección del modo de partida:");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_3.setAlignmentY(Component.BOTTOM_ALIGNMENT);
-		lblNewLabel_3.setBounds(933, 44, 199, 14);
-		frmSabelotodo.getContentPane().add(lblNewLabel_3);
+		JLabel modoPartidaLabel = new JLabel("Selección del modo de partida:");
+		modoPartidaLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		modoPartidaLabel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		modoPartidaLabel.setBounds(846, 33, 199, 14);
+		frmSabelotodo.getContentPane().add(modoPartidaLabel);
 		
 		JPanel panel_partida = new JPanel();
-		panel_partida.setBackground(Color.WHITE);
+		panel_partida.setBackground(SystemColor.inactiveCaptionBorder);
 		panel_partida.setBounds(10, 143, 1243, 431);
 		frmSabelotodo.getContentPane().add(panel_partida);
+		panel_partida.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(627, 70, 147, 20);
-		frmSabelotodo.getContentPane().add(textField);
-		textField.setColumns(10);
+		JComboBox comboBoxOpciones = new JComboBox();
+		comboBoxOpciones.setBounds(1035, 128, 164, 22);
+		panel_partida.add(comboBoxOpciones);
+		
+		JScrollPane scrollTablero = new JScrollPane();
+		scrollTablero.setBounds(30, 39, 481, 381);
+		panel_partida.add(scrollTablero);
+		
+		JTextPane tableroPane = new JTextPane();
+		tableroPane.setToolTipText("aquí se muestra el tablero");
+		tableroPane.setFont(new Font("Dialog", Font.PLAIN, 19));
+		tableroPane.setBackground(UIManager.getColor("List.dropCellBackground"));
+		scrollTablero.setViewportView(tableroPane);
+		
+		JLabel tableroLabel = new JLabel("TABLERO");
+		tableroLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		tableroLabel.setBounds(201, 11, 108, 29);
+		panel_partida.add(tableroLabel);
+		
+		JLabel preguntaLabel = new JLabel("PREGUNTA");
+		preguntaLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		preguntaLabel.setBounds(552, 11, 146, 29);
+		panel_partida.add(preguntaLabel);
+		
+		JLabel selecciónLabel = new JLabel("Selección de la respuesta");
+		selecciónLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		selecciónLabel.setBounds(1035, 102, 164, 14);
+		panel_partida.add(selecciónLabel);
+		
+		JLabel lblNewLabel_5_2 = new JLabel("DADO");
+		lblNewLabel_5_2.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblNewLabel_5_2.setBounds(542, 228, 68, 29);
+		panel_partida.add(lblNewLabel_5_2);
+		
+		JLabel lblNewLabel_5_3 = new JLabel("Resultado:");
+		lblNewLabel_5_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_5_3.setBounds(1035, 162, 100, 29);
+		panel_partida.add(lblNewLabel_5_3);
+		
+		JTextPane infoCasilla = new JTextPane();
+		infoCasilla.setFont(new Font("Dialog", Font.PLAIN, 16));
+		infoCasilla.setBounds(542, 382, 344, 38);
+		panel_partida.add(infoCasilla);
+		
+		JLabel casillaLabel = new JLabel("CASILLA ACTUAL");
+		casillaLabel.setBounds(542, 348, 223, 22);
+		panel_partida.add(casillaLabel);
+		
+		JTextPane dado = new JTextPane();
+		dado.setFont(new Font("Dialog", Font.PLAIN, 73));
+		dado.setBounds(617, 228, 118, 108);
+		panel_partida.add(dado);
+		
+		JLabel resLabel = new JLabel("res");
+		resLabel.setFont(new Font("Dialog", Font.BOLD, 14));
+		resLabel.setBounds(1138, 164, 93, 28);
+		panel_partida.add(resLabel);
+		
+		JButton responderPregunta = new JButton("RESPONDER");
+		responderPregunta.setBounds(1035, 39, 164, 51);
+		panel_partida.add(responderPregunta);
+		
+		JLabel tiempoLabel = new JLabel("TIEMPO:");
+		tiempoLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		tiempoLabel.setBounds(786, 11, 100, 29);
+		panel_partida.add(tiempoLabel);
+		
+		timeLog = new JTextField();
+		timeLog.setFont(new Font("Dialog", Font.PLAIN, 19));
+		timeLog.setBounds(882, 11, 75, 38);
+		panel_partida.add(timeLog);
+		timeLog.setColumns(10);
+		
+		JButton seguirPartida = new JButton("DESPLAZAR FICHA");
+		seguirPartida.setBounds(747, 267, 139, 69);
+		panel_partida.add(seguirPartida);
+		
+		JButton tiraDado = new JButton("TIRAR");
+		tiraDado.setBounds(750, 228, 88, 26);
+		panel_partida.add(tiraDado);
+		
+		JPanel InformacionPanel = new JPanel();
+		InformacionPanel.setBackground(SystemColor.info);
+		InformacionPanel.setBounds(898, 209, 333, 211);
+		panel_partida.add(InformacionPanel);
+		InformacionPanel.setLayout(null);
+		
+		JLabel lblNewLabel_5_2_2 = new JLabel("INFORMACIÓN DE LA PARTIDA");
+		lblNewLabel_5_2_2.setBounds(6, 5, 320, 28);
+		InformacionPanel.add(lblNewLabel_5_2_2);
+		lblNewLabel_5_2_2.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		
+		JTextPane nombreJugador = new JTextPane();
+		nombreJugador.setFont(new Font("Dialog", Font.PLAIN, 20));
+		nombreJugador.setBackground(SystemColor.control);
+		nombreJugador.setBounds(135, 45, 186, 43);
+		InformacionPanel.add(nombreJugador);
+		
+		JTextPane puntuacion = new JTextPane();
+		puntuacion.setFont(new Font("Dialog", Font.PLAIN, 22));
+		puntuacion.setBackground(SystemColor.menu);
+		puntuacion.setBounds(182, 100, 139, 43);
+		InformacionPanel.add(puntuacion);
+		
+		JTextPane vidas = new JTextPane();
+		vidas.setFont(new Font("Dialog", Font.PLAIN, 22));
+		vidas.setBackground(SystemColor.menu);
+		vidas.setBounds(182, 155, 139, 44);
+		InformacionPanel.add(vidas);
+		
+		JLabel jugadorLabel = new JLabel("JUGADOR");
+		jugadorLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+		jugadorLabel.setBounds(42, 46, 86, 21);
+		InformacionPanel.add(jugadorLabel);
+		
+		JLabel puntuacionLabel = new JLabel("PUNTUACIÓN");
+		puntuacionLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+		puntuacionLabel.setBounds(52, 100, 112, 21);
+		InformacionPanel.add(puntuacionLabel);
+		
+		JLabel vidasLabel = new JLabel("VIDAS");
+		vidasLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+		vidasLabel.setBounds(78, 155, 86, 21);
+		InformacionPanel.add(vidasLabel);
+		
+		JScrollPane scrollPregunta = new JScrollPane();
+		scrollPregunta.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPregunta.setBounds(523, 52, 500, 150);
+		panel_partida.add(scrollPregunta);
+		
+		JTextPane preguntaPane = new JTextPane();
+		scrollPregunta.setViewportView(preguntaPane);
+		preguntaPane.setFont(new Font("Dialog", Font.PLAIN, 17));
+		preguntaPane.setBackground(new Color(240, 255, 255));
+		
+		insertaJugador = new JTextField();
+		insertaJugador.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		insertaJugador.setBounds(567, 59, 210, 33);
+		frmSabelotodo.getContentPane().add(insertaJugador);
+		insertaJugador.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Nombre del jugador:");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_4.setBounds(627, 46, 165, 14);
+		lblNewLabel_4.setBounds(557, 33, 165, 14);
 		frmSabelotodo.getContentPane().add(lblNewLabel_4);
 		
-		JButton btnNewButton_3 = new JButton("validar");
-		btnNewButton_3.setBounds(784, 69, 89, 23);
-		frmSabelotodo.getContentPane().add(btnNewButton_3);
+		JButton registraJugador = new JButton("registrar");
+		registraJugador.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		registraJugador.setBounds(688, 93, 89, 39);
+		frmSabelotodo.getContentPane().add(registraJugador);
 		
-		JPanel panel_interno = new JPanel();
+		JButton inicioPartida = new JButton("INICIAR PARTIDA");
+		inicioPartida.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		inicioPartida.setBounds(1070, 33, 165, 72);
+		frmSabelotodo.getContentPane().add(inicioPartida);
+		
+		JLabel developpers = new JLabel("PF ENTERTAINMENT");
+		developpers.setFont(new Font("Dialog", Font.BOLD, 16));
+		developpers.setBounds(71, 584, 258, 19);
+		frmSabelotodo.getContentPane().add(developpers);
+		
+		JLabel partidaLabel = new JLabel("Estado:");
+		partidaLabel.setBounds(601, 586, 49, 19);
+		frmSabelotodo.getContentPane().add(partidaLabel);
+		
+		JLabel estadoLabel = new JLabel("estadoPartida");
+		estadoLabel.setBounds(662, 587, 99, 16);
+		frmSabelotodo.getContentPane().add(estadoLabel);
+		
+		
 	}
 }
---
+++ código/prPFentertainment/src/main/java/GUI/Controlador.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import Modelo.Partida;
+
+public class Controlador implements ActionListener {
+
+	private Partida partida;
+	private JFrame ventana;
+	
+	public Controlador() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
